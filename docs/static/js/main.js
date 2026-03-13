a173c5d61a145daf209d93c77c3b742658910c12
Maxim Belyayev <maximbelyayev@gmail.com>
2026-03-13 09:06:48 -0400

    Docs: add dependencies

--- docs/static/js/main.js
+++ docs/static/js/main.js
@@ -1,8 +1,18 @@
 import 'vite/modulepreload-polyfill'
 import "@/css/main.css"
 
+import htmx from 'htmx.org';
+import 'htmx.org/dist/ext/hx-alpine-compat';
+
 import Alpine from 'alpinejs'
+import anchor from '@alpinejs/anchor'
+import collapse from '@alpinejs/collapse'
+import focus from '@alpinejs/focus'
+import resize from '@alpinejs/resize'
 
 window.Alpine = Alpine
-
+Alpine.plugin(anchor)
+Alpine.plugin(collapse)
+Alpine.plugin(focus)
+Alpine.plugin(resize)
 Alpine.start();
